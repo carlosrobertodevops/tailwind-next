cbe0ae4eb545be00f0368dc605abd0669c30b892
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-04-28 07:55:30 -0300

    Estrutura do Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="min-h-screen">
+          <aside></aside>
+          <main>
+            {children}
+          </main>
+          </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,31 @@
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
-      <div className="max-w-app">
-        <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">
-          Rapidy build modern websites without ever leaving your HTML.
-        </h1>
-        <p className="mt-4 dark:text-slate-400">
-          Beautifully designed, expertly crafted components and templates, built
-          by the makers of Tailwind CSS. The perfect starting point for your
-          next project.
-        </p>
-        <button
-          className="disabled: mt-4  cursor-not-allowed rounded-md bg-sky-500 px-4 py-2 font-medium text-white enabled:hover:bg-sky-400 disabled:opacity-60
-          dark:text-sky-950"
-        >
-          Sign In
-        </button>
-      </div>
-    </div>
+    <h1>
+      hello word
+    </h1>
   )
 }
+
+
+// export default function Home() {
+//   return (
+//     <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+//       <div className="max-w-app">
+//         <h1 className="text-3xl font-bold sm:text-4xl lg:text-5xl">
+//           Rapidy build modern websites without ever leaving your HTML.
+//         </h1>
+//         <p className="mt-4 dark:text-slate-400">
+//           Beautifully designed, expertly crafted components and templates, built
+//           by the makers of Tailwind CSS. The perfect starting point for your
+//           next project.
+//         </p>
+//         <button
+//           className="disabled: mt-4  cursor-not-allowed rounded-md bg-sky-500 px-4 py-2 font-medium text-white enabled:hover:bg-sky-400 disabled:opacity-60
+//           dark:text-sky-950"
+//         >
+//           Sign In
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
