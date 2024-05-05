fe3b0c99e34994c5aa5ac70e24889af67271f3bd
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-05-05 11:53:31 -0300

    Widget de Espaço Usado

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,20 @@
-import { Search } from 'lucide-react'
-import { MainNavigation } from '../MainNavigation'
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
 import { Logo } from './Logo'
+import { NavItem } from './NavItem'
+
 export function Siderbar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
       <div className="flex w-full items-center gap-2 rounded-lg border-zinc-300 px-3 py-2 shadow-sm">
@@ -14,7 +25,50 @@ export function Siderbar() {
         ></input>
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settigs" icon={Cog} />
+        </nav>
+
+        <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+          <div className="space-y-1">
+            <span className="text-sm/5 font-semibold text-violet-700">
+              Uded space
+            </span>
+            <p className="text-sm/5 font-semibold text-violet-500">
+              Your team has used 80% of your avaliable space. Need more ?
+            </p>
+          </div>
+
+          <div className="h-2 rounded-full bg-violet-100">
+            <div className="h-2 w-4/5 rounded-full bg-violet-600"></div>
+          </div>
+          <div className="space-x-3">
+            <button
+              type="button"
+              claassName="text-sm font-medium text-violet-500"
+            >
+              Dismiss
+            </button>
+            <button
+              type="button"
+              claassName="text-sm font-medium text-violet-700"
+            >
+              Upgrade plan
+            </button>
+          </div>
+        </div>
+      </div>
     </aside>
   )
 }
