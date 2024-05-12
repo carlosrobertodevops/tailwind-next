390b7b1d7ffcf89ea6e066130fcd038ab340d3ab
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-05-12 15:05:48 -0300

    Perfil do Usuário

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,19 +1,23 @@
+import { LogOut } from 'lucide-react'
 export function Profile() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="grid-cols-profile flex items-center gap-3">
       <img
         src="https://github.com/carlosrobertodevops.png"
         className="h-10 w-10 rounded-full"
         alt=""
       />
-      <div className="flex flex-col">
+      <div className="flex flex-col truncate">
         <span className="text-sm font-semibold text-zinc-700">
           Carlos Roberto
         </span>
-        <span className="text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500">
           carlos.silva@carloscodemy.store
         </span>
       </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
     </div>
   )
 }
