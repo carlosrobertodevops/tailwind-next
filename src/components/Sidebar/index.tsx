9f37b9f2c82e5487555dd857caef1a8d5189e74f
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-05-18 12:22:07 -0300

    Pattern de Composição

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,19 +13,19 @@ import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { Profile } from './Profile'
 import { UsedSpaceWidgetProps } from './UsedSpaceWidget'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
 
 export function Siderbar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="flex w-full items-center gap-2 rounded-lg border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          placeholder="Seach"
-        ></input>
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500"></Search>
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
